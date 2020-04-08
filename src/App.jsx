7c7cb35a6f2e2fa794c25eabaabdb767122af6c2
akumiq <kun.miqdad23@gmail.com>
2020-04-08 10:12:02 +0700

    test api onHandlePost

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ class App extends Component {
   }
 
   onHandlePost = () => {
-    axios.post('http://localhost:4000/posts', this.state.postDataSantri)
+    axios.post('https://my-json-server.typicode.com/akumiq/crudEa/posts', this.state.postDataSantri)
       .then(() => {
         this.onGetDataSantri()
         this.setState({
